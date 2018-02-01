fa2d1b4db528b64222368f9d007cfb3a5f246fba
JordanRBurke <jordanrobertburke@gmail.com>
2018-02-01 16:44:20 -0500

    AddingIfStatements Questions

---
+++ src/com/company/CarModel.java
@@ -0,0 +1,97 @@
+package com.company;
+
+public class CarModel {
+
+    private int year;
+    private String carName;
+    private String wheelType;
+    private int speedMPH;
+    private String model;
+    private int sizeCar;
+    private int milageCar;
+    private double weightCar;
+    private String color;
+    private String engine;
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    // Car options
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getWheelType() {
+        return wheelType;
+    }
+
+    public void setWheelType(String wheelType) {
+        this.wheelType = wheelType;
+    }
+
+    public int getSpeedMPH() {
+        return speedMPH;
+    }
+
+    public void setSpeedMPH(int speedMPH) {
+        this.speedMPH = speedMPH;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSizeCar() {
+        return sizeCar;
+    }
+
+    public void setSizeCar(int sizeCar) {
+        this.sizeCar = sizeCar;
+    }
+
+    public int getMilageCar() {
+        return milageCar;
+    }
+
+    public void setMilageCar(int milageCar) {
+        this.milageCar = milageCar;
+    }
+
+    public double getWeightCar() {
+        return weightCar;
+    }
+
+    public void setWeightCar(double weightCar) {
+        this.weightCar = weightCar;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,101 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner input = new Scanner(System.in);
+//        String userResponse = "Custom";
+//        String userResponse2 = "Pre-made";
+
+
+        CarModel carModel = new CarModel();
+        CarModel carOptions = new CarModel();
+        CarModel carOptions2 = new CarModel();
+
+        //SpecificCars
+        carModel.setYear(2018);
+        carModel.setCarName("AVALON");
+        carModel.setMilageCar(21/30);
+        carModel.setWheelType("505 Torq Thrust 2");
+        carModel.setWeightCar(3508);
+        carModel.setModel("XLE Premium");
+
+        //RandomParts
+
+        //Options 1
+        carOptions.setColor("Blue");
+        carOptions.setEngine("BOXER");
+        carOptions.setWeightCar(2580);
+        carOptions.setWheelType("White Spoke");
+
+
+
+
+        // Options 2
+        carOptions2.setColor("Orange");
+        carOptions2.setWheelType("Rock Crusher");
+        carOptions2.setWeightCar(3723);
+        carOptions2.setModel("Sports Classic");
+
+
+        System.out.println("Would you like a custom car or a pre-made car?");
+        System.out.println("Please enter Pre-made or Custom for the car (It has to be spelled correctly)");
+        if (input.nextLine().equals("Custom")) {
+            System.out.println("You have choose a Custom Car!!");
+            System.out.println("Would you like a Orange or Blue car?");
+            if (input.nextLine().equals("Orange")) {
+                System.out.println("You have choose ORANGE!!");
+                System.out.println("What tires would you like Rock Crusher or White Spoke?");
+            } else if (input.nextLine().equals("Rock Crusher")) {
+                System.out.println("You have choose Rock Crusher");
+
+
+            } else if (input.nextLine().equals("Blue")) {
+                System.out.println("You have choose BLUE!!");
+
+            }
+            System.out.println("What tires would you like Rock Crusher or White Spoke?");
+            if (input.nextLine().equals("Rock Crusher")) {
+                System.out.println("You have choose Rock Crusher!!");
+            }
+
+
+        } else if (input.nextLine().equals("Pre-made")) {
+            System.out.println("You have choose a Pre-made Car!!!");
+            System.out.println("You have the options of AVALON....");
+        } else {
+            System.out.println("Error answer not recognized!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
